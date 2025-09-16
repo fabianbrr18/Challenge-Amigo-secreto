983c49033511015c5c30ce3f969e75fd9d7e9503
fabianbrr18 <fafecic@gmail.com>
2025-09-15 20:32:04 -0500

    ajustes pre-finales js

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let arrayNombres = []
-function almacenarNombres(nombre) {
-    arrayNombres.push(nombre);
-    let listaAmigos = document.getElementById('listaAmigos');
-    listaAmigos.innerHTML = arrayNombres.map(amigo => `<li>${amigo}</li>`).join('');
 
+function mostrarAmigos() {
+    let listaAmigos = document.getElementById('listaAmigos')
+    listaAmigos.innerHTML = "";
+
+    for (let i = 0; i < arrayNombres.length; i++) {
+        let li = document.createElement("li");
+
+        li.textContent = arrayNombres[i];
+
+        listaAmigos.appendChild(li);
+    }
 }
+
 function agregarAmigo() {
     let elementoNombre = document.getElementById('amigo');
     let nombre = elementoNombre.value;
@@ -13,13 +21,22 @@ function agregarAmigo() {
     if (nombre == '') {
         alert('El nombre no puede ser vacio')
     } else {
-        almacenarNombres(nombre);
+        arrayNombres.push(nombre);
+        mostrarAmigos();
         elementoNombre.value = '';
     }
 }
 
 function sortearAmigo() {
+
+    if (arrayNombres.length === 0) {
+        alert('Primero debes agregar un amigo para sortear')
+        return;
+    }
     let sorteo = Math.floor(Math.random() * arrayNombres.length);
     let sortearAmigoSecreto = arrayNombres[sorteo];
-    console.log(sortearAmigoSecreto);
+
+    let valorSorteo = document.getElementById('resultado');
+
+    valorSorteo.innerHTML = `<li> Tu amgido secreto es: ${sortearAmigoSecreto}</li>`;
 }
